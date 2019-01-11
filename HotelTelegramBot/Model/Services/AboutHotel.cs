f51a802af9a43c7555d296c9e5327f80f74238fc
GABAnich <doofixxx@gmail.com>
2019-01-11 08:55:04 +0200

    Model/AboutHotel.cs -> Model/Services/AboutHotel.cs

---
+++ HotelTelegramBot/Model/Services/AboutHotel.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+
+namespace HotelTelegramBot.Model
+{
+    class AboutHotel
+    {
+        public static string InfoAboutHotel
+        {
+            get
+            {
+                XmlDocument xDoc = new XmlDocument();
+                xDoc.Load(@"..\..\Model\AboutHotel.xml");
+
+                return xDoc.SelectSingleNode("//data/infoAboutHotel").InnerText;
+            }
+        }
+
+        public static string ImageAboutHotel
+        {
+            get
+            {
+                XmlDocument xDoc = new XmlDocument();
+                xDoc.Load(@"..\..\Model\AboutHotel.xml");
+
+                return xDoc.SelectSingleNode("//data/imageAboutHotel").InnerText;
+            }
+        }
+    }
+}
