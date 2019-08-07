15f9bd20917a4ce9f45765a4cdde496616ba5f74
GABAnich <doofixxx@gmail.com>
2019-08-07 17:14:58 +0300

    Extract SetUpChatResponder method

--- HotelTelegramBot/Controller/MessageController.cs
+++ HotelTelegramBot/Controller/MessageController.cs
@@ -13,28 +13,25 @@ namespace HotelTelegramBot.Controller
         internal static void OnMessageAsync(object sender, MessageEventArgs e)
         {
             Chat chat = e.Message.Chat;
-
-            if (!chatResponders.Exists(c => c.Item1 == chat.Id))
-            {
-                chatResponders.Add(new Tuple<long, ChatResponder>(chat.Id, new ChatResponder(chat, new Start())));
-                return;
-            }
-
-            chatResponders.FirstOrDefault(c => c.Item1 == chat.Id).Item2.ReceiveMessageAsync(e);
+            SetUpChatResponder(e, chat);
         }
 
         internal static async void OnCallbackQueryAsync(object sender, CallbackQueryEventArgs e)
         {
             Chat chat = e.CallbackQuery.Message.Chat;
             int messageId = e.CallbackQuery.Message.MessageId;
+            await Program.botClient.DeleteMessageAsync(chat, messageId);
+            SetUpChatResponder(e, chat);
+        }
 
+        private static void SetUpChatResponder(EventArgs e, Chat chat)
+        {
             if (!chatResponders.Exists(c => c.Item1 == chat.Id))
             {
                 chatResponders.Add(new Tuple<long, ChatResponder>(chat.Id, new ChatResponder(chat, new Start())));
                 return;
             }
 
-            await Program.botClient.DeleteMessageAsync(chat, messageId);
             chatResponders.FirstOrDefault(c => c.Item1 == chat.Id).Item2.ReceiveMessageAsync(e);
         }
     }
