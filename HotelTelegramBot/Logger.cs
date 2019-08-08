310c3b783b1400886047d2014811291cfcd526e6
GABAnich <doofixxx@gmail.com>
2019-08-08 14:01:22 +0300

    Generalize Logger

--- HotelTelegramBot/Logger.cs
+++ HotelTelegramBot/Logger.cs
@@ -1,36 +1,40 @@
 ﻿using System;
 using Telegram.Bot.Args;
+using Telegram.Bot.Types;
 
 namespace HotelTelegramBot
 {
     class Logger
     {
-        internal static void Log(string chatPosition, MessageEventArgs e)
+        internal static void Log(string chatPosition, EventArgs e)
         {
-            string text = "" +
-                    $"{e.Message.Date.ToShortDateString()} " +
-                    $"{e.Message.Date.ToShortTimeString()} | " +
-                    $"{chatPosition} | " +
-                    $"{e.Message.Chat.Id} | " +
-                    $"{e.Message.Chat.Username} |" +
-                    $"{e.Message.Chat.LastName} " +
-                    $"{e.Message.Chat.FirstName} : " +
-                    $"{e.Message.Text}\n";
-            System.IO.File.AppendAllText(ConfigTelegramBot.LogFile, text);
-            Console.WriteLine(text);
-        }
+            Message message;
+            string messageText;
 
-        internal static void Log(string chatPosition, CallbackQueryEventArgs e)
-        {
+            if ((e as MessageEventArgs) != null)
+            {
+                message = (e as MessageEventArgs).Message;
+                messageText = message.Text;
+            }
+            else if ((e as CallbackQueryEventArgs) != null)
+            {
+                message = (e as CallbackQueryEventArgs).CallbackQuery.Message;
+                messageText = (e as CallbackQueryEventArgs).CallbackQuery.Data;
+            }
+            else
+            {
+                return;
+            }
+
             string text = "" +
-                    $"{e.CallbackQuery.Message.Date.ToShortDateString()} " +
-                    $"{e.CallbackQuery.Message.Date.ToShortTimeString()} | " +
+                    $"{message.Date.ToShortDateString()} " +
+                    $"{message.Date.ToShortTimeString()} | " +
                     $"{chatPosition} | " +
-                    $"{e.CallbackQuery.Message.Chat.Id} | " +
-                    $"{e.CallbackQuery.Message.Chat.Username} |" +
-                    $"{e.CallbackQuery.Message.Chat.LastName} " +
-                    $"{e.CallbackQuery.Message.Chat.FirstName} : " +
-                    $"{e.CallbackQuery.Data}\n";
+                    $"{message.Chat.Id} | " +
+                    $"{message.Chat.Username} |" +
+                    $"{message.Chat.LastName} " +
+                    $"{message.Chat.FirstName} : " +
+                    $"{messageText}\n";
             System.IO.File.AppendAllText(ConfigTelegramBot.LogFile, text);
             Console.WriteLine(text);
         }
